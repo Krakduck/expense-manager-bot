46b9c7773fe8e59d3a0ffaa090b4134c18baa4ca
Krakduck <162735744+Krakduck@users.noreply.github.com>
2026-06-28 22:50:14 +0300

    Initial commit

---
+++ aiogram bot.py
@@ -0,0 +1,323 @@
+import asyncio
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import random
+import os
+from database_iogram import UserDatabase
+
+bot_token = os.getenv('BOT_TOKEN')
+admin_id = int(os.getenv('ADMIN_ID'))
+
+async def main():
+    bot = Bot(token=bot_token)
+    dp=Dispatcher()
+    db=UserDatabase()
+
+    # 2. Подключаем обработчики
+    dp.include_router(router)
+
+    # 3. А вот так мы запускаем создание таблицы при старте!
+    print("Создаем таблицы в базе данных...")
+    await db.create_table()
+
+    # 4. Запускаем бота (polling)
+    print("Бот успешно запущен!")
+    await dp.start_polling(bot,db=db)
+
+# Создаем роутер (маршрутизатор) для наших обработчиков
+router = Router()
+
+class UpdateSettings(StatesGroup): # класс создается как контейнер. он нужен для логического объединения функций, относящихся к регистрации
+    waiting_for_data = State()# это и есть создание конкретного «статуса» (состояния). Когда пользователь пишет боту, aiogram смотрит на этого пользователя и спрашивает у своей памяти:
+                          # «У Романа сейчас включен маркер Registration.waiting_for_data?». Если да, то сообщение отправляется в функцию регистрации, а не в игру или главное меню.
+                         #Грубо говоря, aiogram внутри себя превращает это в обычную строку, например: "Registration:waiting_for_data".Там лежит объект-указатель, имя которого строго привязано к твоему классу.
+
+class Delete(StatesGroup):
+    waiting_for_delete = State()
+
+class Spent(StatesGroup):
+    waiting_for_spent = State()
+
+def format_user_profile(user_data: dict) -> str:
+    return (
+        f"👤 **Профиль пользователя**\n\n"
+        f"ID: `{user_data['telegram_id']}`\n"
+        f"Имя: {user_data['username']}\n"
+        f"Статус: {'Администратор' if user_data['is_admin'] else 'Пользователь'}"
+    )
+
+async def profile(message: Message):
+    # 1. Создаем объект конструктора клавиатуры
+    builder = InlineKeyboardBuilder()
+    # 2. Добавляем кнопки (текст и callback_data — то, что бот поймает при нажатии)
+    builder.add(InlineKeyboardButton(
+        text="👤 Мой профиль",
+        callback_data="user_info"
+    ))
+    builder.add(InlineKeyboardButton(
+        text="📝 Изменить имя",
+        callback_data="set_profile"
+    ))
+    # 3. Указываем, как распределить кнопки (например, по 1 в ряд)
+    builder.adjust(1)
+    # 4. Отправляем сообщение вместе с клавиатурой
+    await message.answer(
+        "Выбери, что тебя интересует 👇",
+        reply_markup=builder.as_markup()  # Превращаем билдер в готовую разметку
+    )
+
+async def debts(message: Message,db: UserDatabase):
+    users= await db.get_all_users_id()
+    balances={}
+    mes=''
+    if users:
+        for user in users:
+            balances[user] = 0
+        transactions = await db.get_transaction(message.chat.id)
+        for trans in transactions:
+            if trans['debtor_id'] is None:
+                balances[trans['buyer_id']] += trans['amount']
+                for user in users:
+                     balances[user] -= trans['amount']/len(users)
+            else:
+                balances[trans['buyer_id']] += trans['amount']
+                balances[trans['debtor_id']] -= trans['amount']
+        named_balances = {}
+        for user_id, balance in balances.items():
+            user_data = await db.get_user(user_id)
+            if user_data:
+                # Берём имя пользователя из БД
+                name = user_data['username']
+                named_balances[name] = balance
+
+        debtors = [[u, abs(b)] for u, b in balances.items() if b < 0]
+        creditors = [[u, b] for u, b in balances.items() if b > 0]
+
+        # Пока у нас есть должники и кредиторы
+        while debtors and creditors:
+            # Берем первых из списка
+            debtor = debtors[0]
+            creditor = creditors[0]
+
+            # Находим минимальную сумму для перевода между ними
+            amount_to_pay = min(debtor[1], creditor[1])
+
+            # Записываем, кто кому переводит
+            mes += f"👤 ID {debtor[0]} ➡️ должен перевести {amount_to_pay:.2f} руб. ➡️ ID {creditor[0]}\n"
+
+            # Вычитаем переведенную сумму из их текущего долга/кредита
+            debtor[1] -= amount_to_pay
+            creditor[1] -= amount_to_pay
+
+            # Если долг или кредит закрыт — удаляем участника из списка
+            if debtor[1] == 0:
+                debtors.pop(0)
+            if creditor[1] == 0:
+                creditors.pop(0)
+        if not mes.strip():
+            mes = "✅ Никто никому ничего не должен! Все в расчете."
+        await message.answer(mes)
+
+#---------------------------------------------COMMANDS------------------------------------------------------------------
+
+@router.message(Command('start'))
+async def start_reg(message: Message, db: UserDatabase):
+    if await db.get_user(message.from_user.id) is None:
+        user_id = message.from_user.id
+        name=f'Пользователь {user_id}'
+        is_admin = False
+        if user_id == 1422346075:
+            is_admin = True
+        await db.add_user(user_id,is_admin,name)
+        await message.answer('Пожалуйста, измените имя в настройках профиля, чтобы участники беседы понимали кто Вы')
+    await profile(message)
+
+@router.message(Command("profile"))
+async def call_menu(message: Message):
+    await profile(message)
+
+@router.message(Command("del_profile"))
+async def del_profile(message: Message, state: FSMContext):
+    if message.from_user.id == admin_id:
+        builder = InlineKeyboardBuilder()
+        builder.add(InlineKeyboardButton(text="Снести всю базу", callback_data="delete_all"))
+        builder.add(InlineKeyboardButton(text="Удалить себя", callback_data="delete_admin "))
+        builder.adjust(1)
+        await message.answer('Что именно удалить?', reply_markup=builder.as_markup())
+    else:
+        await message.answer("Вы уверены, что хотите удалить аккаунт? Напишите да/нет")
+        await state.set_state(Delete.waiting_for_delete)
+
+@router.message(Command("spent"))
+async def spent(message: Message, state: FSMContext):
+    await message.answer('Напиши сообщение в формате "сумма описание имя"\n'
+                         'К примеру, 1000 пицца все,\n'
+                         '25 рублей вода @123.\n'
+                         'Учти, что в поле "имя" нужно писать либо юзернейм человека, либо "все"')
+    await state.set_state(Spent.waiting_for_spent)
+
+@router.message(Command("debts"))
+async def debt_await(message: Message, db: UserDatabase):
+    await debts(message, db)
+
+@router.message(Command("clear_debt"))
+async def clear_debt(message: Message, db: UserDatabase):
+    if message.from_user.id == admin_id:
+        if await db.clear_debt(message.chat.id):
+            await message.answer('Все долги списаны!')
+        else:
+            await message.answer('Что-то пошло не так')
+    else:
+        await message.answer('Долги списать может только админ')
+
+@router.message(Command("random"))
+async def random_user(message: Message, db: UserDatabase):
+    users = await db.get_all_users_id()
+    if users:
+        random_id = random.choice(users)
+        name = await db.get_user(random_id)
+        await message.answer(name["username"])
+
+@router.message(Command("all"))
+async def all_user(message: Message, db: UserDatabase):
+    mes=''
+    users = await db.get_all_users_id()
+    for user_id in users:
+        # Получаем данные пользователя по его ID
+        user_data = await db.get_user(user_id)
+        if user_data:
+            name = user_data['username']
+            # Теперь у нас есть и имя для текста, и ID для ссылки
+            mes += f"[{name}](tg://user?id={user_id}) "
+    await message.answer(mes, parse_mode="Markdown")
+
+@router.message(Command("help"))
+async def help_user(message: Message):
+    await message.answer('💰 Бот-Счетовод: Как это работает?\n'
+                         'Я помогаю компаниям делить совместные расходы и автоматически рассчитываю, кто кому и сколько должен перевести.\n'
+                         '📊 Основные команды:\n'
+                         '🔹 /del_profile — Удалить учетной записи.\n'
+                         '🔹 /spent — Добавить новую трату. Бот спросит сумму, описание и на кого записать расход (на конкретного человека или на всех).\n'
+                         '🔹 /debts — Показать итоговый баланс и оптимальную цепочку переводов для закрытия долгов.\n'
+                         '🔹 /clear_debt — Сбросить всю историю трат (для админов).\n'
+                         '🔹 /random — Выбрать случайного пользователя.\n'
+                         '🔹 /all — Упомянуть всех участников чата для быстрого сбора.\n'
+                         '⚙️ Попробуй также команду /profile, чтобы настроить свой профиль!')
+
+#-------------------------------------STATE-----------------------------------------------------------------------------
+
+@router.message(UpdateSettings.waiting_for_data)# Ловим сообщение ТОЛЬКО если пользователь находится в этом состоянии.
+async def process_registration_data(message: Message, state: FSMContext, db: UserDatabase):# Когда мы пишем await state.set_state(Registration.waiting_for_data), мы говорим боту: «Запиши в базу данных памяти для этого юзера строку "Registration:waiting_for_data"».
+    if await db.update_user_param(message.from_user.id,'username',message.text):
+        await message.answer("Имя успешно изменено!")
+        await state.clear()
+    else:
+        await message.answer("Ошибка! Что-то пошло не так")
+
+@router.message(Delete.waiting_for_delete)
+async def delete_user(message: Message, state: FSMContext, db: UserDatabase):
+    if message.text.strip().lower() == "да":
+        await db.delete_user(message.from_user.id)
+        await state.clear()
+    elif message.text.strip().lower() == "нет":
+        await message.answer("Операция прекращена")
+        await state.clear()
+    else:
+        await message.answer("Напишите да/нет")
+
+@router.message(Spent.waiting_for_spent)
+async def process_spent(message: Message, state: FSMContext, db: UserDatabase):
+    data = message.text.strip().split()
+    if len(data) == 3:
+        amount_str,description,target=data
+        buyer_id= message.from_user.id
+        # 1. Проверяем сумму
+        try:
+            amount = float(amount_str)
+        except ValueError:
+            await message.answer("❌ Сумма должна быть числом!")
+            return
+        # 2. Определяем должника
+        if target.lower() == "все":
+            debtor_id = None
+        else:
+            # Ищем ID пользователя среди упоминаний в сообщении
+            debtor_id = None
+            if message.entities:
+                for entity in message.entities:
+                    if entity.type == "text_mention":  # Если у пользователя нет @username, но его имя кликабельно
+                        debtor_id = entity.user.id
+                    elif entity.type == "mention":  # Если это обычный @username
+                        # Для обычного mention нам придется спросить нашу БД,
+                        # успел ли этот @username хоть раз отметиться в боте
+                        debtor_id = await db.get_user_by_username(target)
+        # 3. Записываем в базу
+        if target.lower() != "все" and debtor_id is None:
+             await message.answer("❌ Я не знаю этого пользователя. Пусть он сначала напишет мне в ЛС /start")
+             return
+
+        await db.add_transaction(message.chat.id, buyer_id, amount, description, debtor_id)
+        await message.answer(f"✅ Трата успешно записана!")
+        await state.clear()
+    else:
+        await message.answer("❌ Неверный формат. Нужно: `Сумма Описание Кому`")
+
+#-------------------------------------CALLBACK--------------------------------------------------------------------------
+
+@router.callback_query(F.data == "delete_admin")
+async def delete_admin(callback: CallbackQuery, db: UserDatabase):
+    await callback.answer()
+    if await db.delete_user(callback.from_user.id):
+        await callback.message.answer("Ваш аккаунт удален")
+    else:
+        await callback.message.answer("Что-то пошло не так")
+
+@router.callback_query(F.data == "delete_all")
+async def delete_all(callback: CallbackQuery, db: UserDatabase):
+    await callback.answer()
+    if await db.delete_all_users():
+        await callback.message.answer("База успешна удалена")
+    else:
+        await callback.message.answer("Что-то пошло не так")
+
+@router.callback_query(F.data == "user_info")
+async def process_user_info(callback: CallbackQuery, db: UserDatabase):
+    # Гасим анимацию загрузки на кнопке
+    await callback.answer()
+
+    user = await db.get_user(callback.from_user.id)
+    if user:
+        text = format_user_profile(user)  # Используем общую функцию
+        await callback.message.answer(text)
+    else:
+        await callback.message.answer("❌ Вы не зарегистрированы. Напишите /start")
+
+@router.callback_query(F.data == "set_profile")
+async def process_user_info(callback: CallbackQuery, state: FSMContext):  # <- Просим aiogram дать нам state
+    # Гасим анимацию загрузки на кнопке
+    await callback.answer()
+
+    # Отправляем сообщение от имени колбэка
+    await callback.message.answer(
+        "👋 Давайте поменяем имя!\n\n"
+        "Пришлите мне ваше имя.\n"
+    )
+    # Включаем маркер ожидания
+    await state.set_state(UpdateSettings.waiting_for_data)# Метод set_state() принимает только один основной параметр — это объект состояния, который мы создали (например, Registration.waiting_for_data).
+    # Он берет id текущего пользователя (из чата) и записывает в базу данных памяти (хранилище FSM) строчку вида: «Пользователь Роман (ID: 12345) сейчас находится в состоянии Registration:waiting_for_data».
+    # Бот отправляет сообщение, завершает выполнение функции cmd_reg и засыпает. Он не ждет ответа внутри этой функции. Он освобождает ресурсы. Но теперь в памяти бота зафиксировано, что ты «в процессе регистрации».
+
+    #Когда мы пишем await state.set_state(Registration.waiting_for_data), мы говорим боту: «Запиши в базу данных памяти для этого юзера строку "Registration:waiting_for_data"».
+
+# Этот хэндлер поймает сообщение, только если у пользователя НЕТ активного состояния
+@router.message()
+async def echo_message(message: Message):
+    await message.answer("Я тебя не понимаю. Напиши /menu.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
---
+++ database_iogram.py
@@ -0,0 +1,184 @@
+import aiosqlite
+import logging
+# ========== НАСТРОЙКА ЛОГИРОВАНИЯ ==========
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+class UserDatabase:
+    def __init__(self):
+        # Определяем путь к БД
+        #self.db_path = '/data/bot_users.db'
+        self.db_path = 'bot_users.db'
+
+    async def create_table(self):
+        async with aiosqlite.connect(self.db_path) as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute('''
+                                    CREATE TABLE IF NOT EXISTS users ( 
+                                        telegram_id INTEGER PRIMARY KEY,
+                                        is_admin BOOLEAN NOT NULL,
+                                        username TEXT NOT NULL
+                                    )
+                                ''')
+                await cursor.execute('''
+                                    CREATE TABLE IF NOT EXISTS expenses (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        chat_id INTEGER NOT NULL,
+                                        buyer_id INTEGER NOT NULL,
+                                        amount REAL NOT NULL,
+                                        description TEXT,
+                                        debtor_id INTEGER, -- Здесь будет ID друга или None (если на всех)
+                                        FOREIGN KEY (buyer_id) REFERENCES users (telegram_id),
+                                        FOREIGN KEY (debtor_id) REFERENCES users (telegram_id)
+                                    ) ''')
+                await connection.commit()
+
+    async def add_user(self, telegram_id, is_admin, username):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                     await cursor.execute(
+                         'INSERT INTO users (telegram_id, is_admin, username) VALUES (?, ?, ?)',
+                         (telegram_id, is_admin, username)
+                     )
+                     await connection.commit()
+            return True
+        except Exception as e:
+            logger.info(f"Ошибка добавления: {e}")
+            return False
+
+    async def get_user(self, telegram_id):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    await cursor.execute(
+                        'SELECT * FROM users WHERE telegram_id = ?',
+                        (telegram_id,)
+                    )
+                    result =await cursor.fetchone()
+
+            if result:
+                logger.info(f"RESULT ИЗ БАЗЫ: {result}")
+                return {
+                    'telegram_id': result[0],
+                    'is_admin': result[1],
+                    'username': result[2],       #на выходе словарь будет
+                }
+            return None
+        except Exception as e:
+            logger.info(f"Ошибка поиска: {e}")
+            return None
+
+    async def get_user_by_username(self, username):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    await cursor.execute(
+                        'SELECT * FROM users WHERE username = ?',
+                        (username,)
+                    )
+                    result =await cursor.fetchone()
+
+            if result:
+                logger.info(f"RESULT ИЗ БАЗЫ: {result}")
+                # В result порядок такой же как в CREATE TABLE:
+                # (telegram_id, is_admin, username, password)
+                return {
+                    'telegram_id': result[0],
+                }
+            return None
+        except Exception as e:
+            logger.info(f"Ошибка поиска: {e}")
+            return None
+
+    async def get_all_users_id(self):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    await cursor.execute("SELECT telegram_id FROM users")
+                    data = await cursor.fetchall()
+
+            return [row[0] for row in data]  # Вернет список вида: [djigurda, роман]
+        except Exception as e:
+            logger.info(f"Ошибка при получении статистики: {e}")
+            return []
+
+    async def update_user_param(self, telegram_id, column_name, new_value):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    query = f"UPDATE users SET {column_name} = ? WHERE telegram_id = ?"
+                    await cursor.execute(query, (new_value, telegram_id))
+                    await connection.commit()
+
+            return True
+        except Exception as e:
+            logger.info(f"Ошибка при обновлении {column_name}: {e}")
+            return False
+
+    async def delete_user(self, telegram_id):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    query = f"DELETE FROM users WHERE telegram_id = ?"
+                    await cursor.execute(query, (telegram_id,))
+                    await connection.commit()
+
+            return True
+        except Exception as e:
+            logger.info(f"ошибка {telegram_id}: {e}")
+            return False
+
+    async def delete_all_users(self):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    query = f"DELETE FROM users"
+                    await cursor.execute(query)
+                    await connection.commit()
+
+            return True
+        except Exception as e:
+            logger.info(f"ошибка:{e}")
+            return False
+
+
+    async def add_transaction(self,chat_id, buyer_id, amount, description, debtor_id):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                     await cursor.execute(
+                         'INSERT INTO expenses (chat_id, buyer_id, amount, description, debtor_id) VALUES (?, ?, ?, ?, ?)',
+                         (chat_id, buyer_id, amount, description, debtor_id)
+                     )
+                     await connection.commit()
+            return True
+        except Exception as e:
+            logger.info(f"Ошибка добавления: {e}")
+            return False
+
+    async def get_transaction(self,chat_id):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                # Включаем режим, чтобы строки из БД возвращались как словари
+                connection.row_factory = aiosqlite.Row
+                async with connection.cursor() as cursor:
+                    # Запрашиваем ВСЕ поля (*) из таблицы расходов
+                    await cursor.execute("SELECT * FROM expenses WHERE chat_id = ?",(chat_id,))
+                    data = await cursor.fetchall()
+            return data
+        except Exception as e:
+            logger.info(f"Ошибка при получении транзакций: {e}")
+            return []
+
+    async def clear_debt(self,chat_id):
+        try:
+            async with aiosqlite.connect(self.db_path) as connection:
+                async with connection.cursor() as cursor:
+                    await cursor.execute(f"DELETE FROM expenses WHERE chat_id = ?",(chat_id,))
+                    await connection.commit()
+
+            return True
+        except Exception as e:
+            logger.info(f"ошибка:{e}")
+            return False
